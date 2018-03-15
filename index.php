57f123003d10e58f5f68969bd4eaf427808fe8b8
Adri Silva <30940624+AdriJhonson@users.noreply.github.com>
2018-03-15 19:11:24 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php require_once 'templates/header.php' ?>
+
+<p>	</p>
+<div class="container-fluid">
+
+<div class="card">
+  <h5 class="card-header">Operações Básicas</h5>
+  <div class="card-body">
+    <h5 class="card-title">As operações matemáticas básicas são adição, subtração, multiplicação e divisão.</h5>
+    <p class="card-text"> 
+    
+        <div class="estrelas">
+            <strong>Sua pontuação nesse nível: </strong>
+            <!-- Implementar um FOR -->
+            <img src="public/images/star.png">
+        </div>
+
+    </p>
+    <button class="btn btn-success"><i class="fas fa-play"></i> Jogar</button>
+  </div>
+</div>
+
+
+</div>
+
+
+
+
+
+<?php require_once 'templates/footer.php' ?>
---
+++ templates/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <title>Math Learning</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Math Learning</a>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Treino</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Ranking</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#">Sobre</a>
+      </li>
+
+    </ul>
+
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="far fa-user"></i>
+                USUÁRIO
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#"><i class="far fa-user"></i> Meus Dados</a>
+            <a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt"></i> Sair</a>
+            </div>
+        </li>
+      </ul>
+  </div>
+</nav>
